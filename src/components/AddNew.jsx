292ea9528e99a677e50a88525b549341e9ba4a5f
achmadakif <achmadakif7@gmail.com>
2023-10-10 23:12:07 +0700

    adjust some styling

--- src/components/AddNew.jsx
+++ src/components/AddNew.jsx
@@ -14,8 +14,8 @@ const Button = () => {
 const AddNew = () => {
   return (
     <section className={`w-full ${styles.paddingX} ${styles.paddingY} font-inter sticky bottom-0 bg-white`}>
-      <div className="w-full h-14 flex justify-between items-center mb-5 px-5 border-[1px] border-primary rounded-[10px]" >
-        <input type="text" className="h-full focus:outline-none" />
+      <div className="w-full h-14 flex items-center mb-5 px-5 border-[1px] border-primary rounded-[10px]" >
+        <input type="text" className="w-full h-full focus:outline-none" />
         <FontAwesomeIcon icon={faCircleXmark} size="xl" style={{color: "#5438dc",}} />
       </div>
       <Button />
