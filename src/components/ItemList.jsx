eceb5d9ba48334d7ce190949d2a87432602a0e68
achmadakif <achmadakif7@gmail.com>
2023-10-10 23:11:59 +0700

    separated itemList component

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,20 +1,33 @@
+import { useState } from "react"
 import styles from "../style"
 import itemsData from '../static/placeholder'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTrashCan } from '@fortawesome/free-solid-svg-icons'
 
+const Item = (props) => {
+  const [checked, setChecked] = useState('');
+
+  // Destructure itemsData
+  const item = props.item;
+  const {todo, status} = item;
+
+  return (
+    <li style={status ? {textDecoration: 'line-through'} : {} } className={`${styles.paddingY} px-4 flex justify-between items-center rounded-[20px] shadow-xl`}>
+      <input type="checkbox" className="w-[20px] h-[20px]" />
+        {todo}
+      <div className="w-[35px] h-[35px] flex justify-center items-center rounded-full bg-dimRed">
+      <FontAwesomeIcon icon={faTrashCan} style={{color: "#ed0e0e",}} />
+      </div>
+    </li>
+  )
+}
+ 
 const ItemList = () => {
   return (
     <section className={`${styles.paddingX} ${styles.paddingY}`}>
       <ul className="flex flex-col space-y-5 font-inter font-bold text-[18px]">
         {itemsData.map( item => (
-          <li key={item.id} className={`${styles.paddingY} px-4 flex justify-between items-center rounded-[20px] shadow-xl`}>
-            <input type="checkbox" className="w-[20px] h-[20px]" />
-            {item.todo}
-            <div className="w-[35px] h-[35px] flex justify-center items-center rounded-full bg-dimRed">
-              <FontAwesomeIcon icon={faTrashCan} style={{color: "#ed0e0e",}} />
-            </div>
-          </li>
+          <Item key={item.id} item={item} />
         ))}
       </ul>
     </section>
