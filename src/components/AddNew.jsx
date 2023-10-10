9c61a5d650be88cf0e5aa0a69771e6ddd13b1e6d
achmadakif <achmadakif7@gmail.com>
2023-10-10 23:46:02 +0700

    Input component adjustment

--- src/components/AddNew.jsx
+++ src/components/AddNew.jsx
@@ -2,6 +2,15 @@ import styles from "../style"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCircleXmark } from '@fortawesome/free-solid-svg-icons'
 
+const Input = () => {
+  return (
+    <div className="w-full h-14 flex items-center mb-5 px-5 border-[1px] border-primary rounded-[10px]" >
+      <input type="text" className="w-full h-full focus:outline-none" />
+      <FontAwesomeIcon icon={faCircleXmark} size="xl" style={{color: "#5438dc",}} />
+    </div>
+  )
+}
+
 
 const Button = () => {
   return (
@@ -14,10 +23,7 @@ const Button = () => {
 const AddNew = () => {
   return (
     <section className={`w-full ${styles.paddingX} ${styles.paddingY} font-inter sticky bottom-0 bg-white`}>
-      <div className="w-full h-14 flex items-center mb-5 px-5 border-[1px] border-primary rounded-[10px]" >
-        <input type="text" className="w-full h-full focus:outline-none" />
-        <FontAwesomeIcon icon={faCircleXmark} size="xl" style={{color: "#5438dc",}} />
-      </div>
+      <Input />
       <Button />
     </section>
   )
