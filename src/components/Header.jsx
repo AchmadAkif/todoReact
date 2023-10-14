7ee925e511466deba8d081a6d3cdb62fb222741b
achmadakif <achmadakif7@gmail.com>
2023-10-14 13:49:49 +0700

    stats feature

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,14 @@
 import styles from "../style";
 
-const Header = ({onRemoveAllItems}) => {
+const Header = ({onRemoveAllItems, items}) => {
+  const totalItems = items.length
+  const checkedItems = items.filter( item => item.status).length
+
   return (
     <section className={`${styles.paddingX} ${styles.paddingY} flex sm:justify-center justify-between items-center font-inter`}>
       <div>
         <h1 className={`${styles.heading2} leading-[50px]`}>Today's Task</h1>
-        <p className={`${styles.paragraph}`}>(0/3) Completed Task</p>
+        <p className={`${styles.paragraph}`}>({checkedItems}/{totalItems}) Completed Task</p>
       </div>
       <h3 className="font-semibold text-primary cursor-pointer" onClick={() => onRemoveAllItems()}>CLEAR ALL</h3>
     </section>
